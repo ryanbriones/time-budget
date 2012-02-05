40f3c21880908338f6736a9f008e7ca11175ebf2
Stephanie Briones <smbriones@gmail.com>
2012-02-04 22:31:42 -0600

    Added initial styles

--- lib/time_budget/application.rb
+++ lib/time_budget/application.rb
@@ -7,7 +7,7 @@ module TimeBudget
     get("/") do
       days = []
       (0..7).each do |day_number|
-        day_name = Date::DAYNAMES[day_number]
+        day_name = Date::ABBR_DAYNAMES[day_number]
         activities = DB[:activities].filter(day: day_number).all.map do |activity_data|
           OpenStruct.new(activity_data)
         end
